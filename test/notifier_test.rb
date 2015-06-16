df64b056cd63193e85633eaad0f77df9b3702b26
Marek Labos <marek@specle.net>
2015-06-16 19:55:32 +0100

    test: use `subject` in Circuitbox::Notifier tests

--- test/notifier_test.rb
+++ test/notifier_test.rb
@@ -3,19 +3,23 @@ require 'circuitbox/notifier'
 require 'active_support/notifications'
 
 describe Circuitbox::Notifier do
+  subject { Circuitbox::Notifier.new(:yammer, 12) }
+
   it "[notify] sends an ActiveSupport::Notification" do
-    ActiveSupport::Notifications.expects(:instrument).with("circuit_open", circuit: 'yammer:12')
-    Circuitbox::Notifier.new(:yammer, 12).notify(:open)
+    ActiveSupport::Notifications.expects(:instrument)
+      .with("circuit_open", circuit: 'yammer:12')
+    subject.notify(:open)
   end
 
   it "[notify_warning] sends an ActiveSupport::Notification" do
-    ActiveSupport::Notifications.expects(:instrument).with("circuit_warning", { circuit: 'yammer:12', message: 'hello'})
-    Circuitbox::Notifier.new(:yammer, 12).notify_warning('hello')
+    ActiveSupport::Notifications.expects(:instrument)
+      .with("circuit_warning", { circuit: 'yammer:12', message: 'hello' })
+    subject.notify_warning('hello')
   end
 
   it '[gauge] sends an ActiveSupport::Notifier' do
-    ActiveSupport::Notifications.expects(:instrument).with("circuit_gauge", { circuit: 'yammer:12', gauge: 'ratio', value: 12})
-    Circuitbox::Notifier.new(:yammer, 12).metric_gauge(:ratio, 12)
-
+    ActiveSupport::Notifications.expects(:instrument)
+      .with("circuit_gauge", { circuit: 'yammer:12', gauge: 'ratio', value: 12 })
+    subject.metric_gauge(:ratio, 12)
   end
 end
